dfa9f2e23b029549b11c7e97078964504a6e3f11
agustincabrera <agustinhkramer@gmail.com>
2025-06-05 08:06:48 -0300

    removed unused files

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from '@mui/material/styles';
-import { customTheme } from './theme';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./global.css";
+import App from "./App";
+import { ThemeProvider } from "@mui/material/styles";
+import { customTheme } from "./theme";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={customTheme}>
@@ -14,5 +15,3 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 );
-
-reportWebVitals();
